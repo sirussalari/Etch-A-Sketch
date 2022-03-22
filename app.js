2a0277418bfc1ce36a68df5bedb5ff0e45b73862
sirussalari <sirus.salari@gmail.com>
2022-03-21 21:25:06 -0700

    Allow for user to clear drawing and change number of squares in grid

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const container = document.querySelector('#container');
+const button = document.querySelector('button');
 let squares = [];
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    container.appendChild(square);
-    squares.push(square);
+function trail(e) {
+    e.target.style.backgroundColor = 'blue';
 }
 
-squares.forEach(square => {
-    square.addEventListener('mouseover', e => {
-        e.target.style.backgroundColor = 'blue';
+function makeSquares(num, remove) {
+    if (remove) {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+    }
+    for (let i = 0; i < num; i++) {
+        const square = document.createElement('div');
+        container.appendChild(square);
+        squares.push(square);
+    }
+    squares.forEach(square => {
+        square.addEventListener('mouseover', trail);
     })
-})
+}
+
+makeSquares(256, false);
+
+function newGrid() {
+    squares = [];
+    const numOfSides = window.prompt('How many squares per side for the new grid? (Please enter a number < 100) ');
+    container.style.gridTemplateColumns = `repeat(${numOfSides}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${numOfSides}, 1fr)`;
+    makeSquares(numOfSides ** 2, true);
+}
+
+button.addEventListener('click', newGrid);
